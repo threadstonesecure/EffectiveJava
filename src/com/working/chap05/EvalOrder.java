19cdbfac8ece4b5f198c6045207b2443f03aac98
threadstonesecure <threadstonesecure@gmail.com>
2017-07-28 15:54:43 +0530

    EJ-05:Yet another Example

--- src/com/working/chap05/EvalOrder.java
+++ src/com/working/chap05/EvalOrder.java
@@ -2,24 +2,25 @@ package com.working.chap05;
 
 import static java.lang.System.out;
 
-public class EvalOrder{
-  public static void main(String[] args){
+public class EvalOrder {
+	public static void main(String[] args) {
 
-    int j = 2;
-    out.println("Evaluation order of operands:");
-    out.println(eval(j++, " + ") + eval(j++, " * ") * eval(j, "\n"));    // (1)
+		int j = 2;
+		out.println("Evaluation order of operands:");
+		out.println(eval(j++, " + ") + eval(j++, " * ") * eval(j, "\n")); // (1)
 
-    int i = 1;
-    out.println("Evaluation order of arguments:");
-    add3(eval(i++, ", "), eval(i++, ", "), eval(i, "\n")); // (2) Three arguments.
-  }
+		int i = 1;
+		out.println("Evaluation order of arguments:");
+		add3(eval(i++, ", "), eval(i++, ", "), eval(i, "\n")); // (2) Three
+																// arguments.
+	}
 
-  public static int eval(int operand, String str) {        // (3)
-    out.print(operand + str);       // Print int operand and String str.
-    return operand;                 // Return int operand.
-  }
+	public static int eval(int operand, String str) { // (3)
+		out.print(operand + str); // Print int operand and String str.
+		return operand; // Return int operand.
+	}
 
-  public static void add3(int operand1, int operand2, int operand3) {    // (4)
-    out.print(operand1 + operand2 + operand3);
-  }
+	public static void add3(int operand1, int operand2, int operand3) { // (4)
+		out.print(operand1 + operand2 + operand3);
+	}
 }
